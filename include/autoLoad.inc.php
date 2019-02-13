7fc0d6450d98b4feb9157019b84e1934596e1e3b
Sandjay Thiagaradja <sandjay.thiagaradja@etu.unilim.fr>
2019-02-13 15:35:29 +0100

    Branche livraison ok

--- include/autoLoad.inc.php
+++ include/autoLoad.inc.php
@@ -1,5 +1,6 @@
 <?php
-function __autoload($className){
-	$repClasses='classes/';
-	require $repClasses.$className.'.class.php';
+spl_autoload_register(function ($className) {
+    $repClasses='class/';
+    require $repClasses.$className.'.class.php';
 }
+);
