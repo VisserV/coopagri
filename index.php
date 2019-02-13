8816ce54b293a0163fc8e9db59df4ed975347e51
Nathan <nathan.mousset@etu.unilim.fr>
2019-02-13 14:22:22 +0100

    Structuration de l'aborescence

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/stylesheet.css">
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+require_once("include/header.inc.php");
+require_once("include/autoLoad.inc.php");
+require_once("include/config.inc.php");
+require_once("include/functions.inc.php");
+$pdo = new Mypdo();
+?>
+<div id="corps">
+<?php
+require_once("include/texte.inc.php");
+?>
+</div>
+
+<div id="spacer"></div>
+<?php
+require_once("include/footer.inc.php"); ?>
