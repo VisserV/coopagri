f27e65d0c48e052b4e3d5804957af16f7e524a59
Lucie Perruchaud <lucie.perruchaud@etu.unilim.fr>
2019-02-13 14:13:04 +0100

    ajout de fichier et suppr facture

---
+++ include/header.inc.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="fr">
+
+<head>
+
+  <meta charset="utf-8">
+
+  <title></title>
+
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+</head>
+	<body>
+	<div id="header">
+	</div>
