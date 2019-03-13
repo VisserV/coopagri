45d56c9d9e750bff087997b8b6c3d3eea9a522ab
Mael Lascombe <mael.lascombe@hotmail.fr>
2019-03-13 15:11:28 +0100

    Réduction réelle des auditorium (oublier de sauvegarder)

--- js/calendrier.js
+++ js/calendrier.js
@@ -89,18 +89,23 @@ function afficheClt(){
    -----------------------------------------------------------------*/
 
    $('#calendar').fullCalendar({
-     now: moment().add(1,'day'),
+     now: '2018-04-07',
      editable: true, // enable draggable events
      droppable: true, // this allows things to be dropped onto the calendar
      aspectRatio: 1.8,
      scrollTime: '00:00', // undo default 6am scrollTime
      header: {
-       left: 'prev,next',
+       left: 'today prev,next',
        center: 'title',
-       right: 'timelineDay'
+       right: 'timelineDay,timelineThreeDays,agendaWeek,month'
      },
      defaultView: 'timelineDay',
-     
+     views: {
+       timelineThreeDays: {
+         type: 'timeline',
+         duration: { days: 3 }
+       }
+     },
      resourceLabelText: 'Rooms',
      resources: [
        { id: 'a', title: 'Auditorium A' },
@@ -119,19 +124,6 @@ function afficheClt(){
        { id: 'k', title: 'Auditorium K' },
        { id: 'l', title: 'Auditorium L' },
        { id: 'm', title: 'Auditorium M' },
-       { id: 'n', title: 'Auditorium N' },
-       { id: 'o', title: 'Auditorium O' },
-       { id: 'p', title: 'Auditorium P' },
-       { id: 'q', title: 'Auditorium Q' },
-       { id: 'r', title: 'Auditorium R' },
-       { id: 's', title: 'Auditorium S' },
-       { id: 't', title: 'Auditorium T' },
-       { id: 'u', title: 'Auditorium U' },
-       { id: 'v', title: 'Auditorium V' },
-       { id: 'w', title: 'Auditorium W' },
-       { id: 'x', title: 'Auditorium X' },
-       { id: 'y', title: 'Auditorium Y' },
-       { id: 'z', title: 'Auditorium Z' }
      ],
      drop: function(date, jsEvent, ui, resourceId) {
        console.log('drop', date.format(), resourceId);
